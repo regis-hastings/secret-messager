b79185de79abc22961efd9d98f0ec1d8d1fe4f02
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-21 14:01:09 -0700

    toggle message and link form visibility

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault(); // Prevents default behavior of submitting form
 
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
   const input = document.querySelector('#message-input');
   const encrypted = btoa(input.value);
 
-  document.querySelector('#link-input').value = encrypted;
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select(); 
 });
